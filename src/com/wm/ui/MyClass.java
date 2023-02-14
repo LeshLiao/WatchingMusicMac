2d5ae8c703ae9fed13eec11027fafec224063a88
leshliao <leviliao38@gmail.com>
2023-02-15 00:44:52 +0800

    Upload first project file.

---
+++ src/com/wm/ui/MyClass.java
@@ -0,0 +1,12 @@
+package com.wm.ui;
+
+import processing.core.*;
+public class MyClass extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiaa");
+		PApplet.main("com.wm.ui.MyClass");
+	}
+
+}
